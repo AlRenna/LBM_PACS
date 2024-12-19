820beee5ab2a223e58631c85c62b091eb6ba5207
Alessandro Renna <alessandro1.renna@mail.polimi.it>
2024-12-19 16:06:04 +0100

    Solved ZouHe for GPU

--- src/lattice.cpp
+++ src/lattice.cpp
@@ -136,9 +136,16 @@ Lattice::populate_Nodes()
     nodes[index] = Node(node_types[index], coord_xy, ux_in[index], uy_in[index], rho_in[index]);
     if(node_types[index] == NodeType::boundary){
       nodes[index].set_bounce_back_properties(bounce_back_dir[index], bounce_back_delta[index]);
-      nodes[index].set_zou_he_type(*this);
     }
     nodes[index].init_equilibrium();
+  }
+
+  // set zou_he_type after creatinng all the nodes
+  #pragma omp parallel for
+  for(unsigned int index = 0; index<nx*ny; ++index){
+    if(node_types[index] == NodeType::boundary){
+      nodes[index].set_zou_he_type(*this);
+    }
   }
 }
 
